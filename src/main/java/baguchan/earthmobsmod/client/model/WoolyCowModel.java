6465597c248452183cfa942ec03a40eb4f058bd0
baguchan <shurf012@gmail.com>
2021-09-02 22:01:14 +0900

    final fix

--- src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
+++ src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
@@ -20,11 +20,11 @@ public class WoolyCowModel<T extends WoolyCow> extends CowModel<T> {
 		int i = 12;
 		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -6.0F, 8.0F, 8.0F, 6.0F).texOffs(22, 0).addBox("right_horn", -5.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F).texOffs(22, 0).addBox("left_horn", 4.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F), PartPose.offset(0.0F, 4.0F, -8.0F));
 		PartDefinition partdefinition2 = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(18, 4).addBox(-6.0F, -10.0F, -7.0F, 12.0F, 18.0F, 10.0F).texOffs(52, 0).addBox(-2.0F, 2.0F, -8.0F, 4.0F, 6.0F, 1.0F), PartPose.offsetAndRotation(0.0F, 5.0F, 2.0F, ((float)Math.PI / 2F), 0.0F, 0.0F));
-		PartDefinition partdefinition3 = partdefinition2.addOrReplaceChild("body_part", CubeListBuilder.create().texOffs(52, 6).addBox(-42.0F, -1.5F, 30.0F, 3F, 8.0F, 0.0F), PartPose.offsetAndRotation(2.5F, 20.0F, 32.0F, ((float)Math.PI / 2F), -((float)Math.PI / 2F), 0.0F));
-		partdefinition3.addOrReplaceChild("body_part2", CubeListBuilder.create().texOffs(52, 6).addBox(-1.5F, -4.0F, 0.0F, 3F, 8.0F, 0.0F), PartPose.offsetAndRotation(-40.0F, 2.5F, 12.0F, ((float)Math.PI), 0.0F, 0.0F));
+		PartDefinition partdefinition3 = partdefinition2.addOrReplaceChild("body_part", CubeListBuilder.create().texOffs(52, 6).addBox(-42.0F, -1.5F, 30.0F, 3F, 8.0F, 0.0F), PartPose.offsetAndRotation(-2.0F, 20.0F, -49.0F, ((float)Math.PI / 2F), ((float)Math.PI / 2F), 0.0F));
+		partdefinition3.addOrReplaceChild("body_part2", CubeListBuilder.create().texOffs(52, 6).addBox(-1.5F, -4.0F, 0.0F, 3F, 8.0F, 0.0F), PartPose.offsetAndRotation(-41.0F, 2.5F, 12.0F, ((float)Math.PI), 0.0F, 0.0F));
 
-		PartDefinition partdefinition4 = partdefinition2.addOrReplaceChild("body_part", CubeListBuilder.create().texOffs(26, 0).addBox(1.0F, -0.01F, -2.0F, 16.0F, 0.0F, 3.0F), PartPose.offsetAndRotation(6.0F, -10.0F, -8.0F, 0.0F, 0.0F, ((float)Math.PI / 2F)));
-		partdefinition4.addOrReplaceChild("body_part2", CubeListBuilder.create().texOffs(26, 0).addBox(-8.0F, 0.0F, -1.5F, 16.0F, 8.0F, 3.0F), PartPose.offsetAndRotation(9.0F, 12.01F, -0.5F, 0.0F, 0.0F, -((float)Math.PI)));
+		PartDefinition partdefinition4 = partdefinition2.addOrReplaceChild("body_part3", CubeListBuilder.create().texOffs(26, 0).addBox(1.0F, -0.01F, -2.0F, 16.0F, 0.0F, 3.0F), PartPose.offsetAndRotation(6.0F, -10.0F, -8.0F, 0.0F, 0.0F, ((float)Math.PI / 2F)));
+		partdefinition4.addOrReplaceChild("body_part4", CubeListBuilder.create().texOffs(26, 0).addBox(-8.0F, 0.0F, -1.5F, 16.0F, 0.0F, 3.0F), PartPose.offsetAndRotation(9.0F, 12.01F, -0.5F, 0.0F, 0.0F, -((float)Math.PI)));
 
 
 		CubeListBuilder cubelistbuilder = CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F);
