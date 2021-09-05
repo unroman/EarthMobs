1cb7982eb9828f7c5b106c03989ccb87228f6e49
baguchan <shurf012@gmail.com>
2021-09-05 11:57:52 +0900

    wooly cow now can use shear

--- src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
+++ src/main/java/baguchan/earthmobsmod/client/model/WoolyCowModel.java
@@ -10,6 +10,8 @@ import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 
 public class WoolyCowModel<T extends WoolyCow> extends CowModel<T> {
+	private float headXRot;
+
 	public WoolyCowModel(ModelPart modelPart) {
 		super(modelPart);
 	}
@@ -33,5 +35,16 @@ public class WoolyCowModel<T extends WoolyCow> extends CowModel<T> {
 		partdefinition.addOrReplaceChild("right_front_leg", cubelistbuilder, PartPose.offset(-4.0F, 12.0F, -6.0F));
 		partdefinition.addOrReplaceChild("left_front_leg", cubelistbuilder, PartPose.offset(4.0F, 12.0F, -6.0F));
 		return LayerDefinition.create(meshdefinition, 64, 32);
+	}
+
+	public void prepareMobModel(T p_103687_, float p_103688_, float p_103689_, float p_103690_) {
+		super.prepareMobModel(p_103687_, p_103688_, p_103689_, p_103690_);
+		this.head.y = 4.0F + p_103687_.getHeadEatPositionScale(p_103690_) * 4.0F;
+		this.headXRot = p_103687_.getHeadEatAngleScale(p_103690_);
+	}
+
+	public void setupAnim(T p_103692_, float p_103693_, float p_103694_, float p_103695_, float p_103696_, float p_103697_) {
+		super.setupAnim(p_103692_, p_103693_, p_103694_, p_103695_, p_103696_, p_103697_);
+		this.head.xRot = this.headXRot;
 	}
 }
--- src/main/java/baguchan/earthmobsmod/client/render/WoolyCowRenderer.java
+++ src/main/java/baguchan/earthmobsmod/client/render/WoolyCowRenderer.java
@@ -12,7 +12,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class WoolyCowRenderer<T extends WoolyCow> extends MobRenderer<T, WoolyCowModel<T>> {
-	private static final ResourceLocation TEXTURE = new ResourceLocation(EarthMobsMod.MODID, "textures/entity/wooly_cow.png");
+	private static final ResourceLocation TEXTURE = new ResourceLocation(EarthMobsMod.MODID, "textures/entity/wooly_cow/wooly_cow.png");
+	private static final ResourceLocation SHEARED_TEXTURE = new ResourceLocation(EarthMobsMod.MODID, "textures/entity/wooly_cow/wooly_cow_sheared.png");
+
 
 	public WoolyCowRenderer(EntityRendererProvider.Context p_173952_) {
 		super(p_173952_, new WoolyCowModel<>(p_173952_.bakeLayer(ModModelLayers.WOOLY_COW)), 0.5F);
@@ -21,6 +23,6 @@ public class WoolyCowRenderer<T extends WoolyCow> extends MobRenderer<T, WoolyCo
 
 	@Override
 	public ResourceLocation getTextureLocation(T p_110775_1_) {
-		return TEXTURE;
+		return p_110775_1_.isSheared() ? SHEARED_TEXTURE : TEXTURE;
 	}
 }
