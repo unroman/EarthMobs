90e88354dcebbb787637be3ceba2effa577bdf80
baguchan <shurf012@gmail.com>
2022-06-02 20:07:08 +0900

    oops

--- src/main/java/baguchan/earthmobsmod/entity/SkeletonWolf.java
+++ src/main/java/baguchan/earthmobsmod/entity/SkeletonWolf.java
@@ -1,14 +1,7 @@
 package baguchan.earthmobsmod.entity;
 
 import baguchan.earthmobsmod.registry.ModEntities;
-import com.google.common.collect.Lists;
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.NbtUtils;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
@@ -28,24 +21,14 @@ import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.common.Tags;
 
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Optional;
 import java.util.Random;
-import java.util.UUID;
-import java.util.function.Predicate;
 
 public class SkeletonWolf extends Wolf {
-	private static final EntityDataAccessor<Optional<UUID>> DATA_TRUSTED_ID_0 = SynchedEntityData.defineId(SkeletonWolf.class, EntityDataSerializers.OPTIONAL_UUID);
-	private static final EntityDataAccessor<Optional<UUID>> DATA_TRUSTED_ID_1 = SynchedEntityData.defineId(SkeletonWolf.class, EntityDataSerializers.OPTIONAL_UUID);
-
-
 	public SkeletonWolf(EntityType<? extends Wolf> p_30369_, Level p_30370_) {
 		super(p_30369_, p_30370_);
 	}
 
 	protected void registerGoals() {
-		this.goalSelector.addGoal(1, new FloatGoal(this));
 		this.goalSelector.addGoal(2, new SitWhenOrderedToGoal(this));
 		this.goalSelector.addGoal(4, new LeapAtTargetGoal(this, 0.4F));
 		this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
@@ -57,10 +40,10 @@ public class SkeletonWolf extends Wolf {
 		this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
 		this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
 		this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setAlertOthers());
-		this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, livingEntity -> {
-			return !this.trusts(livingEntity.getUUID()) && this.isWorstCondition();
+		this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+		this.targetSelector.addGoal(5, new NonTameRandomTargetGoal<>(this, Player.class, true, livingEntity -> {
+			return this.isWorstCondition();
 		}));
-		this.targetSelector.addGoal(5, new NonTameRandomTargetGoal<>(this, Player.class, true, (Predicate<LivingEntity>) null));
 		this.targetSelector.addGoal(6, new NonTameRandomTargetGoal<>(this, Turtle.class, false, Turtle.BABY_ON_LAND_SELECTOR));
 		this.targetSelector.addGoal(8, new ResetUniversalAngerTargetGoal<>(this, true));
 	}
@@ -68,43 +51,14 @@ public class SkeletonWolf extends Wolf {
 	@Override
 	protected void defineSynchedData() {
 		super.defineSynchedData();
-		this.entityData.define(DATA_TRUSTED_ID_0, Optional.empty());
-		this.entityData.define(DATA_TRUSTED_ID_1, Optional.empty());
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
 		return Wolf.createAttributes().add(Attributes.MAX_HEALTH, 12.0F).add(Attributes.ATTACK_DAMAGE, 4.0D);
 	}
 
-	@Nullable
-	protected List<UUID> getTrustedUUIDs() {
-		List<UUID> list = Lists.newArrayList();
-		list.add(this.entityData.get(DATA_TRUSTED_ID_0).orElse((UUID) null));
-		list.add(this.entityData.get(DATA_TRUSTED_ID_1).orElse((UUID) null));
-		return list;
-	}
-
-	protected void addTrustedUUID(@Nullable UUID p_28516_) {
-		if (this.entityData.get(DATA_TRUSTED_ID_0).isPresent()) {
-			this.entityData.set(DATA_TRUSTED_ID_1, Optional.ofNullable(p_28516_));
-		} else {
-			this.entityData.set(DATA_TRUSTED_ID_0, Optional.ofNullable(p_28516_));
-		}
-	}
-
-	protected boolean trusts(UUID p_28530_) {
-		return this.getTrustedUUIDs().contains(p_28530_);
-	}
-
 	protected boolean isWorstCondition() {
 		return this.getBrightness() < 0.45F;
-	}
-
-	public void tame(Player p_21829_) {
-		super.tame(p_21829_);
-		if (this.getTrustedUUIDs().isEmpty() || !this.getTrustedUUIDs().contains(p_21829_.getUUID())) {
-			this.addTrustedUUID(p_21829_.getUUID());
-		}
 	}
 
 	@Override
@@ -118,29 +72,6 @@ public class SkeletonWolf extends Wolf {
 		}
 
 		this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(6.0D);
-	}
-
-	public void addAdditionalSaveData(CompoundTag p_28518_) {
-		super.addAdditionalSaveData(p_28518_);
-		List<UUID> list = this.getTrustedUUIDs();
-		ListTag listtag = new ListTag();
-
-		for (UUID uuid : list) {
-			if (uuid != null) {
-				listtag.add(NbtUtils.createUUID(uuid));
-			}
-		}
-
-		p_28518_.put("Trusted", listtag);
-	}
-
-	public void readAdditionalSaveData(CompoundTag p_28493_) {
-		super.readAdditionalSaveData(p_28493_);
-		ListTag listtag = p_28493_.getList("Trusted", 11);
-
-		for (int i = 0; i < listtag.size(); ++i) {
-			this.addTrustedUUID(NbtUtils.loadUUID(listtag.get(i)));
-		}
 	}
 
 	public InteractionResult mobInteract(Player p_30412_, InteractionHand p_30413_) {
@@ -188,14 +119,6 @@ public class SkeletonWolf extends Wolf {
 					itemstack.shrink(1);
 				}
 
-				if (!this.isWorstCondition() && !this.trusts(p_30412_.getUUID())) {
-					if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, p_30412_)) {
-						this.addTrustedUUID(p_30412_.getUUID());
-						this.level.broadcastEntityEvent(this, (byte) 14);
-					} else {
-						this.level.broadcastEntityEvent(this, (byte) 6);
-					}
-				} else {
 					if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, p_30412_)) {
 						this.tame(p_30412_);
 						this.navigation.stop();
@@ -205,7 +128,6 @@ public class SkeletonWolf extends Wolf {
 					} else {
 						this.level.broadcastEntityEvent(this, (byte) 6);
 					}
-				}
 
 				return InteractionResult.SUCCESS;
 			}
