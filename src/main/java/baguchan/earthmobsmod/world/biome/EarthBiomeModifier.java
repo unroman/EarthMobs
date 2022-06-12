a94b3c0375d63845d76a098059b27cf83919778c
baguchan <shurf012@gmail.com>
2022-06-13 06:35:17 +0900

    finally!

--- src/main/java/baguchan/earthmobsmod/world/biome/EarthBiomeModifier.java
+++ src/main/java/baguchan/earthmobsmod/world/biome/EarthBiomeModifier.java
@@ -5,10 +5,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ModifiableBiomeInfo;
 
@@ -16,7 +16,7 @@ public record EarthBiomeModifier(HolderSet<Biome> biomes, MobSpawnSettings.Spawn
 								 MobCategory category) implements BiomeModifier {
 	@Override
 	public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
-		if (phase == Phase.ADD && this.biomes.contains(biome) && biome.containsTag(BiomeTags.IS_OVERWORLD)) {
+		if (phase == Phase.ADD && this.biomes.contains(biome) && !biome.containsTag(Tags.Biomes.IS_MODIFIED)) {
 			builder.getMobSpawnSettings().addSpawn(category, spawn);
 
 		}
