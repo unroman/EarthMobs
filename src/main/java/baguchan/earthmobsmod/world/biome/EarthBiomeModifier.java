3452416f6e3bb6e42d0a118a69741467af73596f
baguchan <shurf012@gmail.com>
2022-06-14 12:59:48 +0900

    oops

--- src/main/java/baguchan/earthmobsmod/world/biome/EarthBiomeModifier.java
+++ src/main/java/baguchan/earthmobsmod/world/biome/EarthBiomeModifier.java
@@ -7,17 +7,17 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ModifiableBiomeInfo;
 
-public record EarthBiomeModifier(HolderSet<Biome> biomes, MobSpawnSettings.SpawnerData spawn,
+public record EarthBiomeModifier(HolderSet<Biome> biomes, HolderSet<Biome> blacklist_biomes,
+								 MobSpawnSettings.SpawnerData spawn,
 								 MobCategory category) implements BiomeModifier {
 	@Override
 	public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
-		if (phase == Phase.ADD && this.biomes.contains(biome) && !biome.containsTag(Tags.Biomes.IS_MODIFIED) && (category != MobCategory.MONSTER || !biome.is(Biomes.MUSHROOM_FIELDS))) {
+		if (phase == Phase.ADD && this.biomes.contains(biome) && !biome.containsTag(Tags.Biomes.IS_MODIFIED) && !blacklist_biomes.contains(biome)) {
 			builder.getMobSpawnSettings().addSpawn(category, spawn);
 
 		}
@@ -26,6 +26,7 @@ public record EarthBiomeModifier(HolderSet<Biome> biomes, MobSpawnSettings.Spawn
 	public static Codec<EarthBiomeModifier> makeCodec() {
 		return RecordCodecBuilder.create(builder -> builder
 				.group(Biome.LIST_CODEC.fieldOf("biomes").forGetter(EarthBiomeModifier::biomes),
+						Biome.LIST_CODEC.fieldOf("blacklist_biomes").forGetter(EarthBiomeModifier::blacklist_biomes),
 						MobSpawnSettings.SpawnerData.CODEC.fieldOf("spawn").forGetter(EarthBiomeModifier::spawn),
 						MobCategory.CODEC.fieldOf("mob_category").forGetter(EarthBiomeModifier::category))
 				.apply(builder, EarthBiomeModifier::new));
