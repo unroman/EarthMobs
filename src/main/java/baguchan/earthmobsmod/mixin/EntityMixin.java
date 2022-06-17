f23896485d374b0308f6086d1890545a9e808767
baguchan <shurf012@gmail.com>
2022-06-17 15:37:03 +0900

    fixed cannot move against fluid

--- src/main/java/baguchan/earthmobsmod/mixin/EntityMixin.java
+++ src/main/java/baguchan/earthmobsmod/mixin/EntityMixin.java
@@ -2,12 +2,9 @@ package baguchan.earthmobsmod.mixin;
 
 import baguchan.earthmobsmod.api.IOnMud;
 import baguchan.earthmobsmod.registry.ModFluidTypes;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.extensions.IForgeEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,19 +22,6 @@ public abstract class EntityMixin implements IOnMud, IForgeEntity {
 			this.wasTouchingMud = false;
 		}
 
-	}
-
-	@Shadow
-	public void resetFallDistance() {
-	}
-
-	@Shadow
-	public void clearFire() {
-	}
-
-	@Shadow
-	public boolean updateFluidHeightAndDoFluidPushing(TagKey<Fluid> p_204032_, double p_204033_) {
-		return false;
 	}
 
 	@Override
