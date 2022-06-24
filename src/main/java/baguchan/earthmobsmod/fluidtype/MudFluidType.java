6c87a2756dd99172f56a543a20a788e8efd518e0
baguchan <shurf012@gmail.com>
2022-06-24 15:58:28 +0900

    actually.... mud!

--- src/main/java/baguchan/earthmobsmod/fluidtype/MudFluidType.java
+++ src/main/java/baguchan/earthmobsmod/fluidtype/MudFluidType.java
@@ -1,7 +1,13 @@
 package baguchan.earthmobsmod.fluidtype;
 
 import baguchan.earthmobsmod.EarthMobsMod;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Matrix4f;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MoverType;
@@ -65,6 +71,31 @@ public class MudFluidType extends FluidType {
 			@Override
 			public ResourceLocation getRenderOverlayTexture(Minecraft mc) {
 				return TEXTURE_OVERLAY;
+			}
+
+			@Override
+			public void renderOverlay(Minecraft mc, PoseStack stack) {
+				ResourceLocation texture = this.getRenderOverlayTexture(mc);
+				if (texture == null) return;
+				RenderSystem.setShader(GameRenderer::getPositionTexShader);
+				RenderSystem.enableTexture();
+				RenderSystem.setShaderTexture(0, texture);
+				BufferBuilder buffer = Tesselator.getInstance().getBuilder();
+				BlockPos playerEyePos = new BlockPos(mc.player.getX(), mc.player.getEyeY(), mc.player.getZ());
+				float brightness = LightTexture.getBrightness(mc.player.level.dimensionType(), mc.player.level.getMaxLocalRawBrightness(playerEyePos));
+				RenderSystem.enableBlend();
+				RenderSystem.defaultBlendFunc();
+				RenderSystem.setShaderColor(brightness, brightness, brightness, 0.65F);
+				float uOffset = -mc.player.getYRot() / 64.0F;
+				float vOffset = mc.player.getXRot() / 64.0F;
+				Matrix4f pose = stack.last().pose();
+				buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+				buffer.vertex(pose, -1.0F, -1.0F, -0.5F).uv(4.0F + uOffset, 4.0F + vOffset).endVertex();
+				buffer.vertex(pose, 1.0F, -1.0F, -0.5F).uv(uOffset, 4.0F + vOffset).endVertex();
+				buffer.vertex(pose, 1.0F, 1.0F, -0.5F).uv(uOffset, vOffset).endVertex();
+				buffer.vertex(pose, -1.0F, 1.0F, -0.5F).uv(4.0F + uOffset, vOffset).endVertex();
+				BufferUploader.drawWithShader(buffer.end());
+				RenderSystem.disableBlend();
 			}
 		});
 	}
