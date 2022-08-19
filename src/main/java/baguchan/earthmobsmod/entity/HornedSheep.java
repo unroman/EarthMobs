1dee947183f0a9b0b94d30adafbdeec2691032bc
baguchan <shurf012@gmail.com>
2022-08-19 17:12:45 +0900

    fixed horned sheep breed

--- src/main/java/baguchan/earthmobsmod/entity/HornedSheep.java
+++ src/main/java/baguchan/earthmobsmod/entity/HornedSheep.java
@@ -1,5 +1,6 @@
 package baguchan.earthmobsmod.entity;
 
+import baguchan.earthmobsmod.registry.ModEntities;
 import baguchan.earthmobsmod.registry.ModItems;
 import baguchan.earthmobsmod.registry.ModLootTables;
 import net.minecraft.core.HolderSet;
@@ -9,12 +10,14 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.InstrumentTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -124,6 +127,11 @@ public class HornedSheep extends Sheep {
 
 	public static AttributeSupplier.Builder createAttributes() {
 		return Sheep.createAttributes().add(Attributes.ATTACK_DAMAGE, 4.0D).add(Attributes.ATTACK_KNOCKBACK, (double) 0.5F);
+	}
+
+	@Override
+	public HornedSheep getBreedOffspring(ServerLevel p_149035_, AgeableMob p_149036_) {
+		return ModEntities.HORNED_SHEEP.get().create(p_149035_);
 	}
 
 	@Override
