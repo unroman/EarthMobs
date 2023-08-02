2125ef8853fbc93d9bba3ea397fe65940b8c82a7
baguchan <shutux500@gmail.com>
2023-08-02 17:09:18 +0900

    fix #44

--- src/main/java/baguchan/earthmobsmod/message/SyncFishMessage.java
+++ src/main/java/baguchan/earthmobsmod/message/SyncFishMessage.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -38,19 +39,20 @@ public class SyncFishMessage {
 
     public static boolean handle(SyncFishMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
         NetworkEvent.Context context = contextSupplier.get();
-
-        context.enqueueWork(() -> {
-            Level level = Minecraft.getInstance().level;
-            if (level == null) {
-                return;
-            }
-            Entity entity = level.getEntity(message.entityId);
-            if (entity != null) {
-                if (entity instanceof TropicalSlime tropicalSlime) {
-                    tropicalSlime.setFishData(message.compoundTag);
+        if (context.getDirection().getReceptionSide() == LogicalSide.CLIENT) {
+            context.enqueueWork(() -> {
+                Level level = Minecraft.getInstance().player.level();
+                if (level == null) {
+                    return;
                 }
-            }
-        });
+                Entity entity = level.getEntity(message.entityId);
+                if (entity != null) {
+                    if (entity instanceof TropicalSlime tropicalSlime) {
+                        tropicalSlime.setFishData(message.compoundTag);
+                    }
+                }
+            });
+        }
 
         return true;
     }
