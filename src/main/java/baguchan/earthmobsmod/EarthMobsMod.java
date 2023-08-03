94ae9973b9b3b34cd292bfb4ed98fc502e2d848d
baguchan <shutux500@gmail.com>
2023-08-03 21:30:42 +0900

    1.20.1-8.0.3

--- src/main/java/baguchan/earthmobsmod/EarthMobsMod.java
+++ src/main/java/baguchan/earthmobsmod/EarthMobsMod.java
@@ -66,7 +66,6 @@ public class EarthMobsMod {
 		ModEffects.POTION.register(modBus);
 		ModItems.ITEMS.register(modBus);
 		ModRecipes.RECIPE_SERIALIZERS.register(modBus);
-		ModLootModifiers.LOOT_MODIFIERS.register(modBus);
 
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
